181a0495c00ce0bedec8d13546e432e8a8b4ff79
lycheeonline <35208300+lycheeonline@users.noreply.github.com>
2022-08-28 12:36:38 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -25,14 +25,14 @@ export const config = {
     
     }
 
-// {{date.DATA}}  
-// 城市：{{city.DATA}}  
-// 天气：{{weather.DATA}}  
-// 最低气温: {{min_temperature.DATA}}  
-// 最高气温: {{max_temperature.DATA}}  
-// 今天是我们恋爱的第{{love_day.DATA}}天
+ {{date.DATA}}  
+城市：{{city.DATA}}  
+天气：{{weather.DATA}}  
+最低气温: {{min_temperature.DATA}}  
+最高气温: {{max_temperature.DATA}}  
+今天是我们恋爱的第{{love_day.DATA}}天
 // 
-// {{birthday_message.DATA}}
+ {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
 // {{note_ch.DATA}}

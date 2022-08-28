9c045a9078571d3319adc96e42c6c8b7dcecedb3
lycheeonline <35208300+lycheeonline@users.noreply.github.com>
2022-08-28 12:14:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ export const config = {
     // 模板消息id
     templateId: "kZThLm1RbMRayCKZmJX3KBbfOjW3lX5H7ZpkzA0zcrY",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: ["oKMUE5wRy-yGT4pYhh3J2o-8F7rw"],
+    user: [oKMUE5wRy-yGT4pYhh3J2o-8F7rw],
      
     // 信息配置
     // 所在省份
